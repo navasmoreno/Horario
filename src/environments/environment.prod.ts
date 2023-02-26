cc4d2a5904713dba75ae2cff7540fc788bdb55ef
Rodrigo <rodrigo.navas.arriaga@gmail.com>
2023-02-26 17:47:39 +0100

    Ajuste de variables de entorno

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: true,
+  dietaPaginacionlocalstorage: "b12fdbfe6592d48f97e62508d987f74f8e1ea1f2",
   dietasRoute: "assets/dietas",
   horariosRoute: "assets/horarios",
-  dietaPaginacionlocalstorage: "20c15e9960a8c7a7e98969c4fc786d83",
   horarios : [
-    {nombre:"Isabel",link:"eb5094069f5e287ad7cfce5fc944f1c6565da2481af83fd5c24568e2481d09e3"},
-    {nombre:"Rodrigo",link:"c9fd92c735c7609969a0ab48b6dc2fda85a06e135196571c06708222baf5a2e7"}
+    {nombre:"Isabel",link:"isabel"},
+    {nombre:"Rodrigo",link:"rodrigo"}
   ],
   dietas : [
-    {nombre:"Dieta del bocadillo",link:"dietabocadillo1"},
-    {nombre:"Dieta del bocadillo 2",link:"dietabocadillo2"},
+    {nombre:"Dieta del bocadillo (semanas)",link:"dietabocadillo1"},
+    {nombre:"Dieta del bocadillo (días)",link:"dietabocadillo2"},
   ],
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,8 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
+  dietaPaginacionlocalstorage: "b4ee9a4c1e3e1da8127c2cf58cc65c731c857aff",
   dietasRoute: "assets/dietas",
   horariosRoute: "assets/horarios",
-  dietaPaginacionlocalstorage: "20c15e9960a8c7a7e98969c4fc786d83",
   horarios : [
     {nombre:"Isabel",link:"isabel"},
     {nombre:"Rodrigo",link:"rodrigo"}
@@ -16,12 +12,3 @@ export const environment = {
     {nombre:"Dieta del bocadillo (días)",link:"dietabocadillo2"},
   ],
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
