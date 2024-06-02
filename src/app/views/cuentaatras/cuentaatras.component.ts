1f493ea2749768979f1f9cf48f774ae8a725c4ab
Rodrigo <navas.moreno.devs@gmail.com>
2024-06-02 11:19:40 +0200

    Poner musica

--- src/app/views/cuentaatras/cuentaatras.component.ts
+++ src/app/views/cuentaatras/cuentaatras.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { faMusic, faPlay } from '@fortawesome/free-solid-svg-icons';
 import * as moment from 'moment';
+import { environment } from 'src/environments/environment.prod';
 
 
 @Component({
@@ -16,16 +18,28 @@ export class CuentaatrasComponent implements OnInit {
   exit = moment('2024-07-06 11:00:00');
   duration: moment.Duration = moment.duration(this.arrive.diff(this.now));
   interval: NodeJS.Timer | undefined;
+  haypermisos: boolean = false;
+  audio: HTMLAudioElement = new Audio();
+  showplay: boolean=false;
+  iconos = { play: faMusic }
   constructor() { }
 
   ngOnInit(): void {
+    this.audio.src = "../../assets/hero.m4a";
+    this.audio.load();
+    var permisos = localStorage.getItem(environment.permisosCuentaAtras);
+    if (permisos != null) {
+      this.loadPermisos(parseInt(permisos));
+    }
+
     this.setText();
     this.interval = setInterval(() => {
       this.now.add(1, 'second');
-      this.duration.subtract(1,'second');
+      this.duration.subtract(1, 'second');
       this.run();
     }, 1000)
   }
+
   run() {
     if (this.now > this.arrive && this.now < this.exit) {
       this.message1 = 'Disfrutando...'
@@ -42,11 +56,38 @@ export class CuentaatrasComponent implements OnInit {
     }
   }
   setText() {
-    this.message1 = this.duration.months()>0 ? `${this.duration.months()} ${(this.duration.months() != 1 ? 'MESES' : 'MES')}` : '';
+    this.message1 = this.duration.months() > 0 ? `${this.duration.months()} ${(this.duration.months() != 1 ? 'MESES' : 'MES')}` : '';
     this.message2 = this.duration.days() > 0 ? `${this.duration.days()} ${(this.duration.days() != 1 ? 'DIAS' : 'DIA')}` : '';
-    this.message3 = `${(this.duration.hours()<=9 ? '0' : '')}${this.duration.hours()}`;
-    this.message3 += `:${(this.duration.minutes()<=9 ? '0' : '')}${this.duration.minutes()}`;
-    this.message3 += `:${(this.duration.seconds()<=9 ? '0' : '')}${this.duration.seconds()}`;
+    this.message3 = `${(this.duration.hours() <= 9 ? '0' : '')}${this.duration.hours()}`;
+    this.message3 += `:${(this.duration.minutes() <= 9 ? '0' : '')}${this.duration.minutes()}`;
+    this.message3 += `:${(this.duration.seconds() <= 9 ? '0' : '')}${this.duration.seconds()}`;
 
+  }
+  permisos(value: number) {
+    console.log(value);
+    if (value == -1) {
+      this.haypermisos = false;
+    } else {
+      localStorage.setItem(environment.permisosCuentaAtras,value.toString());
+      this.loadPermisos(value);
+    }
+  }
+  loadPermisos(permiso: number) {
+    this.haypermisos = true;
+    console.log(permiso,this.audio.paused);
+    if (permiso == 1) {
+      this.audio.play().then(()=>{
+
+      }).catch(error=>{
+        this.showplay=true;
+      });
+    } else {
+      this.audio.pause();
+      this.audio.currentTime = 0;
+    }
+  }
+  playMusic() {
+    this.audio.play();
+    this.showplay=false;
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
   dietaPaginacionlocalstorage: "b12fdbfe6592d48f97e62508d987f74f8e1ea1f2",
-  valenciaslocalstorage : "b12fdbfe6592d48f97e62508d987f74f8e1ea1f2askliwe"
+  valenciaslocalstorage : "b12fdbfe6592d48f97e62508d987f74f8e1ea1f2askliwe",
+  permisosCuentaAtras: "ad0d5a4993f57582a214a0a289ba8bdf71a80e5a"
 };
 export const firebaseConfig = {
   apiKey: "AIzaSyAiZMOA8Xwah60YhRmmcsqz7u6Yv1eABuA",
